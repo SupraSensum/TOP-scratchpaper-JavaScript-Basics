2c8a3a69f3e80c97ddc11dd4cbc72b3a087f9c9a
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-07-26 15:41:04 -0300

    Create login using switch and if statements

--- js/fundamentals-part-2.js
+++ js/fundamentals-part-2.js
@@ -1,1 +1,45 @@
 // alert(Infinity);
+
+let userName = prompt("Enter username");
+let userPassword;
+
+// // Login prompting using switch cases
+// switch (userName) {
+//     case "Admin":
+//         switch (userPassword = prompt("Enter password")) {
+//             case "TheMaster":
+//                 alert("Welcome!");
+//                 break;
+//             case null:
+//             case "":
+//                 alert("Cancelled");
+//                 break;
+//             default:
+//                 alert("Wrong password");
+//                 break;
+//         }
+//         break;
+//     case null:
+//     case "":
+//         alert("Cancelled");
+//         break;
+//     default:
+//         alert("I don't know you");
+//         break;
+// }
+
+// Login prompting using if statements and conditional operators
+if (userName == "Admin") {
+    userPassword = prompt("Enter password");
+    if (userPassword == "TheMaster") {
+        alert("Welcome!");
+    } else if (userPassword == null || userPassword == "") {
+        alert("Cancelled");
+    } else {
+        alert("Wrong password");
+    }
+} else if (userName == null || userName == "") {
+    alert("Cancelled");
+} else {
+    alert("I don't know you");
+}
