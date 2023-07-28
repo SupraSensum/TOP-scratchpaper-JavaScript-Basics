241474b97f125a702816047db890ef286eedee57
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-07-28 19:36:38 -0300

    Have mind blown from simple keylogger...

---
+++ js/fundamentals-part-3.js
@@ -0,0 +1,26 @@
+// let numberBeforeAdding7 = prompt("Gimme a numbah",'');
+// let multiplier1 = prompt("Gimme anotha numbah",'');
+// let multiplier2 = prompt("Gimme anotha one",'');
+// let someDudesString1 = prompt("Gimme yo string",'');
+// let someDudesString2 = prompt("Gimme yo last string",'');
+
+// function add7 (theNumberWeGonAdd7To) {
+//     return +theNumberWeGonAdd7To + 7;
+// }
+
+// function multiply (a, b) {
+//     return a * b;
+// }
+
+// function capitalize (someString) {
+//     return someString.charAt(0).toUpperCase() + someString.slice(1);
+// }
+
+// function lastLetter (someString) {
+//     return someString.charAt(someString.length-1);
+// }
+
+// console.log(add7(numberBeforeAdding7));
+// console.log(multiply(multiplier1, multiplier2));
+// console.log(capitalize(someDudesString1));
+// console.log(lastLetter(someDudesString2));
