802d0faa6248bc0c405ca66dd9a8a7626311b19a
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-08-09 18:11:08 -0300

    Get transition end events working

--- js/drum-kit-vanilla-js.js
+++ js/drum-kit-vanilla-js.js
@@ -1,3 +1,4 @@
+// Associate element id with keycode for easy reference to <audio> id's
 const drumItems = [
    [document.querySelector('#clap'), 'code-a'],
    [document.querySelector('#hihat'), 'code-s'],
@@ -10,23 +11,63 @@ const drumItems = [
    [document.querySelector('#tink'), 'code-l']
 ]
 
+// Keyboard event controller
 window.addEventListener('keydown', (event) => {
    const audioFile = document.querySelector(`#code-${event.key}`);
-   playTheAudioFile (audioFile);
+   let drumItemElement = '';
+   
+   // Find the related element
+   for (let i = 0; i < drumItems.length; i++) {
+      if (drumItems[i][1] === `code-${event.key}`) {
+         playAndAnimate(audioFile, drumItems[i][0]);
+         console.log("FOUND");
+         return;
+      }
+   }
+
+   console.log("MISSING");
+
+   return;
 });
 
+// Add mouse click event listeners to each drumItem
 drumItems.forEach((drumItem, index) => {
    const drumItemElement = drumItem[0];
    const drumItemKey = drumItem[1];
 
    drumItemElement.addEventListener('click', () => {
       const audioFile = document.querySelector(`#${drumItemKey}`);
-      playTheAudioFile(audioFile);
+      playAndAnimate(audioFile, drumItemElement);
    });
+
+   drumItemElement.addEventListener('transitionend', removeTransition);
 });
 
 function playTheAudioFile (audioFile) {
    if(!audioFile) return;
    audioFile.currentTime = 0;
    audioFile.play();
+   return;
+}
+
+function animateTheThing (theThing) {
+   theThing.classList.add('playing');
+   return;
+}
+
+function playAndAnimate (audioFile, theThing) {
+   playTheAudioFile(audioFile);
+   animateTheThing(theThing);
+
+   return;
+}
+
+function removeTransition (event) {
+   if (event.propertyName !== 'transform') {
+      return;
+   }
+   
+   this.classList.remove('playing');
+
+   return;
 }
